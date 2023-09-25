b412f4ff061c58df1ea2f40c295686a667f3e213
fhmaisonneuve <e2296133@cmaisonnuve.qc.ca>
2023-09-25 13:13:26 -0400

    Updatejson remote=ok Updatejson local=ok

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Presentation from "./components/Presentation";
 import ProductModale from "./components/ProductModale";
 import Nav from "./components/Nav";
 
-
 function App() {
   //GLOBAL
   const [tasks, setTasks] = useState([]);
@@ -15,13 +14,21 @@ function App() {
   const [showModal_ReadOnly, setShowModal_ReadOnly] = useState(false);
   const [showModal_Mode, setShowModal_Mode] = useState(false);
   const [currentItem, setCurrentItem] = useState(null);
+
+  //**
+  // TP_REMOTEVERSION
+  //  * LOCALVAR: use local variable
+  // * LOCALVAR_ASJSON: use local json 
+  // * REMOTEJSON: use remote json
+  //** */
   const TP_REMOTEVERSION = {
     LOCALVAR: "localvar",
     LOCALVAR_ASJSON: "localvar_AsJSON",
     REMOTEJSON: "remoteJSON",
   };
+
   //const SELECTED_TP_REMOTEVERSION=TP_REMOTEVERSION.LOCALVAR;
-  const SELECTED_TP_REMOTEVERSION=TP_REMOTEVERSION.REMOTEJSON;
+  const SELECTEDTP_VERSION = TP_REMOTEVERSION.REMOTEJSON;
 
   ////////////////////////////////////////////////////////
   /////////////   LOAD DATAS
@@ -30,104 +37,112 @@ function App() {
   useEffect(() => {
     const getTasks = async () => {
       //const tasksFromServer = await fetchTasks();
-      if (SELECTED_TP_REMOTEVERSION==TP_REMOTEVERSION.REMOTEJSON) {
+      if (SELECTEDTP_VERSION == TP_REMOTEVERSION.REMOTEJSON) {
         const tasksFromServer = await fetchItemsRemote();
         setTasks(tasksFromServer);
-      }else if (SELECTED_TP_REMOTEVERSION==TP_REMOTEVERSION.LOCALVAR_ASJSON) {
+      } else if (SELECTEDTP_VERSION == TP_REMOTEVERSION.LOCALVAR_ASJSON) {
         const tasksFromServer = await fetchItemsLocalJson();
         setTasks(tasksFromServer);
-      }else{
-
+      } else {
         const tasksFromServer = await fetchItemsLocal();
         setTasks(tasksFromServer);
       }
-   
     };
     getTasks();
   }, []);
 
 
+
   //////////////////////////////////////////////////////////
   // FETCH
-  
+
   //FETCH TASKS LOCAL (VAR)
   const fetchItemsLocal = async () => {
     return [
-        {
-          "id": 1,
-          "nom": "Microsoft Visual Studio",
-          "description": "Un environnement de développement intégré (IDE) puissant pour les développeurs .NET.",
-          "prix": 199.99,
-          "categorie": "IDE"
-        },
-        {
-          "id": 2,
-          "nom": "Adobe Photoshop",
-          "description": "Un logiciel de retouche d'images professionnel utilisé par les designers du monde entier.",
-          "prix": 249.99,
-          "categorie": "Logiciel de retouche"
-        },
-        {
-          "id": 3,
-          "nom": "Python",
-          "description": "Un langage de programmation interprété, polyvalent et largement utilisé dans le développement web et scientifique.",
-          "prix": 0,
-          "categorie": "Langage de programmation"
-        },
-        {
-          "id": 4,
-          "nom": "Microsoft Excel",
-          "description": "Une application de feuilles de calcul puissante utilisée pour l'analyse de données et la gestion de projet.",
-          "prix": 129.99,
-          "categorie": "Logiciel de gestion de données"
-        },
-        {
-          "id": 5,
-          "nom": "React",
-          "description": "Une bibliothèque JavaScript populaire pour la création d'interfaces utilisateur interactives.",
-          "prix": 0,
-          "categorie": "Framework de programmation"
-        },
-        {
-          "id": 6,
-          "nom": "AutoCAD",
-          "description": "Un logiciel de conception assistée par ordinateur (CAO) utilisé dans l'architecture et l'ingénierie.",
-          "prix": 699.99,
-          "categorie": "Logiciel de CAO"
-        },
-        {
-          "id": 7,
-          "nom": "Java",
-          "description": "Un langage de programmation polyvalent et populaire utilisé dans le développement d'applications web et mobiles.",
-          "prix": 0,
-          "categorie": "Langage de programmation"
-        },
-        {
-          "id": 8,
-          "nom": "Sublime Text",
-          "description": "Un éditeur de texte léger et personnalisable apprécié des développeurs.",
-          "prix": 70.0,
-          "categorie": "IDE"
-        },
-        {
-          "id": 9,
-          "nom": "Angular",
-          "description": "Un framework JavaScript pour la création d'applications web dynamiques.",
-          "prix": 0,
-          "categorie": "Framework de programmation"
-        },
-        {
-          "id": 10,
-          "nom": "Adobe Illustrator",
-          "description": "Un logiciel de création de graphiques vectoriels utilisé par les illustrateurs et les designers.",
-          "prix": 239.99,
-          "categorie": "Logiciel de design"
-        }
-      ];
-   
+      {
+        id: 1,
+        nom: "Microsoft Visual Studio",
+        description:
+          "Un environnement de développement intégré (IDE) puissant pour les développeurs .NET.",
+        prix: 199.99,
+        categorie: "IDE",
+      },
+      {
+        id: 2,
+        nom: "Adobe Photoshop",
+        description:
+          "Un logiciel de retouche d'images professionnel utilisé par les designers du monde entier.",
+        prix: 249.99,
+        categorie: "Logiciel de retouche",
+      },
+      {
+        id: 3,
+        nom: "Python",
+        description:
+          "Un langage de programmation interprété, polyvalent et largement utilisé dans le développement web et scientifique.",
+        prix: 0,
+        categorie: "Langage de programmation",
+      },
+      {
+        id: 4,
+        nom: "Microsoft Excel",
+        description:
+          "Une application de feuilles de calcul puissante utilisée pour l'analyse de données et la gestion de projet.",
+        prix: 129.99,
+        categorie: "Logiciel de gestion de données",
+      },
+      {
+        id: 5,
+        nom: "React",
+        description:
+          "Une bibliothèque JavaScript populaire pour la création d'interfaces utilisateur interactives.",
+        prix: 0,
+        categorie: "Framework de programmation",
+      },
+      {
+        id: 6,
+        nom: "AutoCAD",
+        description:
+          "Un logiciel de conception assistée par ordinateur (CAO) utilisé dans l'architecture et l'ingénierie.",
+        prix: 699.99,
+        categorie: "Logiciel de CAO",
+      },
+      {
+        id: 7,
+        nom: "Java",
+        description:
+          "Un langage de programmation polyvalent et populaire utilisé dans le développement d'applications web et mobiles.",
+        prix: 0,
+        categorie: "Langage de programmation",
+      },
+      {
+        id: 8,
+        nom: "Sublime Text",
+        description:
+          "Un éditeur de texte léger et personnalisable apprécié des développeurs.",
+        prix: 70.0,
+        categorie: "IDE",
+      },
+      {
+        id: 9,
+        nom: "Angular",
+        description:
+          "Un framework JavaScript pour la création d'applications web dynamiques.",
+        prix: 0,
+        categorie: "Framework de programmation",
+      },
+      {
+        id: 10,
+        nom: "Adobe Illustrator",
+        description:
+          "Un logiciel de création de graphiques vectoriels utilisé par les illustrateurs et les designers.",
+        prix: 239.99,
+        categorie: "Logiciel de design",
+      },
+    ];
   };
 
-   //FETCH TASKS LOCAL (JSON)
+  //FETCH TASKS LOCAL (JSON)
   const fetchItemsLocalJson = async () => {
     return localData["produits"];
   };
@@ -138,6 +153,8 @@ function App() {
     const data = await res.json();
     return data;
   };
+
+
 
   ////////////////////////////////////////////////////////
   /////////////   ADD DATAS
@@ -178,7 +195,7 @@ function App() {
     // console.log(id)
     const taskToToggle = await fetchItemsRemote(item.id);
     //const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
-    const res = await fetch(`http://localhost:5000/tasks/${item.id}`, {
+    const res = await fetch(`http://localhost:5000/produits/${item.id}`, {
       method: "PUT",
       headers: {
         "content-type": "application/json",
@@ -244,8 +261,16 @@ function App() {
           readOnly={showModal_ReadOnly}
           onOpenModal={openProductModal}
           onCloseModal={closeProductModal}
-          onAdd={addItemLocal}
-          onUpdate={updateItemLocal}
+          onAdd={
+            SELECTEDTP_VERSION === TP_REMOTEVERSION.REMOTEJSON
+              ? addItemRemote
+              : addItemLocal
+          }
+          onUpdate={
+            SELECTEDTP_VERSION === TP_REMOTEVERSION.REMOTEJSON
+              ? updateItemREMOTE
+              : updateItemLocal
+          }
           product={currentItem}
           mode={showModal_Mode}
         />
@@ -253,18 +278,19 @@ function App() {
           <Route
             path="/Produits"
             element={
-              SELECTED_TP_REMOTEVERSION==TP_REMOTEVERSION.LOCALVAR || SELECTED_TP_REMOTEVERSION==TP_REMOTEVERSION.LOCALVAR_ASJSON ? (
-              tasks.length > 0 ? (
-                <ManyTasks
-                  items={tasks}
-                  onDeleteMany={deleteItemLocal}
-                  onViewMany={showModale_editItem}
-                  onAdd={showModale_addItem}
-                />
-              ) : (
-                "Aucun produit à afficher"
-              )
-              ): SELECTED_TP_REMOTEVERSION === TP_REMOTEVERSION.REMOTEJSON ? (
+              SELECTEDTP_VERSION == TP_REMOTEVERSION.LOCALVAR ||
+              SELECTEDTP_VERSION == TP_REMOTEVERSION.LOCALVAR_ASJSON ? (
+                tasks.length > 0 ? (
+                  <ManyTasks
+                    items={tasks}
+                    onDeleteMany={deleteItemLocal}
+                    onViewMany={showModale_editItem}
+                    onAdd={showModale_addItem}
+                  />
+                ) : (
+                  "Aucun produit à afficher"
+                )
+              ) : SELECTEDTP_VERSION === TP_REMOTEVERSION.REMOTEJSON ? (
                 tasks.length > 0 ? (
                   <ManyTasks
                     items={tasks}
